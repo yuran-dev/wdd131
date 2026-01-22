06663bfe786a5cdc19c93384cce559cdca860cef
yuran-dev <yuran.nhangumbe@gmail.com>
2026-01-22 15:47:06 +0200

    commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,10 +2,17 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 document.getElementById('last-modified').textContent = document.lastModified;
 
-// Hamburger menu toggle
+// Hamburger menu
 const hamburger = document.getElementById('hamburger');
 const navList = document.querySelector('.nav-list');
 
 hamburger.addEventListener('click', () => {
     navList.classList.toggle('open');
+
+    // Alterna símbolo ☰ e X
+    if (navList.classList.contains('open')) {
+        hamburger.textContent = '✖';
+    } else {
+        hamburger.textContent = '☰';
+    }
 });
